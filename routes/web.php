c5b0cd28b8411edecd6debb2aeda3ae583239cef
sekieki <driniseki@gmail.com>
2020-12-07 14:00:46 +0100

    Controller and Routes
    Created a Controller that contain functions : index,create,store,edit,update and Destroy and defined the Routes for this Functions in web.php

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::namespace('App\Http\Controllers')->middleware('auth')->group(function(){
+    Route::resource('/articles','ArticlesController',['except'=>['show']]);
+});
+
+
