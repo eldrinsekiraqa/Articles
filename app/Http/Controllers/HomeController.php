4a716f308bfab9932f57d0e060d56c529f384e42
sekieki <driniseki@gmail.com>
2020-12-07 20:24:37 +0100

    Show Articles in Home View
    Added Status when a Article is created (Publish/Pending)
    Added relations between User and Article Model
    Used the Relation between the Models to get the data for both of them
    Changed status to maxium 15 characters

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Articles;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +26,11 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $articles = Articles::with('user')
+            ->where('status','Publish')
+            ->orderByDesc('publish_date')
+            ->get();
+
+        return view('home')->with('articles',$articles);
     }
 }
