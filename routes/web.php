4afa6aa468556e7c0b8c71bf78cbbe7583a45e21
sekieki <driniseki@gmail.com>
2020-12-08 14:21:58 +0100

    Unimplemented the Verification part added an User seeder

--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes(['verify'=>true]);
+Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::namespace('App\Http\Controllers')->middleware('auth','verified')->group(function(){
+Route::namespace('App\Http\Controllers')->middleware('auth')->group(function(){
     Route::resource('/articles','ArticlesController',['except'=>['show','index','update','destroy','edit']]);
     Route::resource('/my_articles','MyArticlesController',['except'=>['show','store','edit','create']]);
-
 });
 
 
